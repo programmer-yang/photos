d9292ef6f7b8dedfb2cd0666bd19baf83ad70034
wb-ly508846 <wb-ly508846@alibaba-inc.com>
2020-03-30 11:14:21 +0800

    udpate api path

--- src/pages/request.js
+++ src/pages/request.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 // import moment from "moment";
 
 const api = Axios.create({
-  baseURL: "http://95.169.16.82:9002/"
+  baseURL: "http://39.108.94.69:9004"
 });
 const getList = page => {
   return api.get(`/api/photos?page=${page}`).then(res => res.data);
